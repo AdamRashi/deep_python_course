258780d13595f327020f74b4e3377d6f30f0ece8
Adam Rashi <adamrashi@mail.ru>
2023-05-24 10:57:13 +0300

    Generator and generator tests updated

--- 01/generator.py
+++ 01/generator.py
@@ -1,13 +1,19 @@
+from io import TextIOWrapper
 from typing import List
 
 
-def filter_lines(filename: str,
-                 words: List[str],
-                 encoding: str = 'utf-8') -> str:
-
+def filter_lines(
+    filename: str | TextIOWrapper, words: List[str], encoding: str = "utf-8"
+) -> str:
     words = [word.lower() for word in words]
-    with open(filename, 'r', encoding=encoding) as file:
-        for line in file:
+    if isinstance(filename, str):
+        with open(filename, "r", encoding=encoding) as file:
+            for line in file:
+                line_words = set(line.lower().split())
+                if line_words.intersection(words):
+                    yield line.strip()
+    else:
+        for line in filename:
             line_words = set(line.lower().split())
             if line_words.intersection(words):
                 yield line.strip()
--- 01/test_generator.py
+++ 01/test_generator.py
@@ -2,22 +2,21 @@ import unittest
 import os
 from generator import filter_lines
 
-TEST_FILE_NAME = 'test_file.txt'
+TEST_FILE_NAME = "test_file.txt"
 
 
 class TestFilterLines(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
         """
         Создаем единожды файл с текстом, который будет использоваться
         практически во всех тестах
         """
-        with open(TEST_FILE_NAME, 'w', encoding='utf-8') as file:
-            file.write('Do you want an apple\n')
-            file.write('Sweet one\n')
-            file.write('And a banana\n')
-            file.write('From Panama\n')
+        with open(TEST_FILE_NAME, "w", encoding="utf-8") as file:
+            file.write("Do you want an apple\n")
+            file.write("Sweet one\n")
+            file.write("And a banana\n")
+            file.write("From Panama\n")
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -26,48 +25,66 @@ class TestFilterLines(unittest.TestCase):
         """
         os.remove(TEST_FILE_NAME)
 
-    def test_several_matches(self, ):
+    def test_several_matches(
+        self,
+    ):
         """
         Случай, когда должны найтись совпадения
         """
-        filter_words = ['apple', 'sweet', 'banana']
-        expected_result = [
-            'Do you want an apple',
-            'Sweet one',
-            'And a banana'
-        ]
+        filter_words = ["apple", "sweet", "banana"]
+        expected_result = ["Do you want an apple", "Sweet one", "And a banana"]
         result = list(filter_lines(TEST_FILE_NAME, filter_words))
         self.assertEqual(result, expected_result)
+
+        # проверим с файловым объектом
+        with open(TEST_FILE_NAME, "r") as file_obj:
+            result = list(filter_lines(file_obj, filter_words))
+            self.assertEqual(result, expected_result)
 
     def test_no_match(self):
         """
         Случай, когда совпадений нет
         """
-        filter_words = ['there', 'is', 'no', 'such', 'words']
+        filter_words = ["there", "is", "no", "such", "words"]
         result = list(filter_lines(TEST_FILE_NAME, filter_words))
         self.assertEqual(result, [])
+
+        # проверим с файловым объектом
+        with open(TEST_FILE_NAME, "r") as file_obj:
+            result = list(filter_lines(file_obj, filter_words))
+            self.assertEqual(result, [])
 
     def test_empty_file(self):
         """
         Случай, когда файл пустой
         """
 
-        file_name = 'empty_file.txt'
-        filter_words = ['apple', 'sweet', 'banana']
-        with open(file_name, 'w', encoding='utf-8'):
+        file_name = "empty_file.txt"
+        filter_words = ["apple", "sweet", "banana"]
+        with open(file_name, "w", encoding="utf-8"):
             pass
-        results = list(filter_lines(file_name, filter_words))
-        os.remove(file_name)
 
+        results = list(filter_lines(file_name, filter_words))
         self.assertEqual(results, [])
+
+        with open(file_name, "r", encoding="utf-8") as file_obj:
+            results = list(filter_lines(file_obj, filter_words))
+            self.assertEqual(results, [])
+
+        os.remove(file_name)
 
     def test_almost_matches(self):
         """
         Случай, когда слова "почти" совпадают со словами в тексте
         """
-        filter_words = ['appel', 'sweetie', 'abanana']
+        filter_words = ["appel", "sweetie", "abanana"]
         result = list(filter_lines(TEST_FILE_NAME, filter_words))
         self.assertEqual(result, [])
+
+        # проверим с файловым объектом
+        with open(TEST_FILE_NAME, "r") as file_obj:
+            result = list(filter_lines(file_obj, filter_words))
+            self.assertEqual(result, [])
 
     def test_no_filter_words(self):
         """
@@ -76,17 +93,17 @@ class TestFilterLines(unittest.TestCase):
         result = list(filter_lines(TEST_FILE_NAME, []))
         self.assertEqual(result, [])
 
+        # проверим с файловым объектом
+        with open(TEST_FILE_NAME, "r") as file_obj:
+            result = list(filter_lines(file_obj, []))
+            self.assertEqual(result, [])
+
     def test_not_matching_case(self):
         """
         Случай, когда регистры слов для поиска и слов в тексте не совпадают
         """
 
-        filter_words = ['aPpLe', 'SWEET', 'banana']
-        expected_result = [
-            'Do you want an apple',
-            'Sweet one',
-            'And a banana'
-        ]
+        filter_words = ["aPpLe", "SWEET", "banana"]
+        expected_result = ["Do you want an apple", "Sweet one", "And a banana"]
         result = list(filter_lines(TEST_FILE_NAME, filter_words))
-
         self.assertEqual(result, expected_result)
