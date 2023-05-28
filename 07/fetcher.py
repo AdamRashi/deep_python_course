a656f666980773981677f3396fd8bdca80486d2b
Adam Rashi <adamrashi@mail.ru>
2023-05-28 23:15:17 +0300

    asynchronious fetcher updated

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -1,39 +1,49 @@
 import asyncio
+import time
 from collections import Counter
 
 import aiohttp
 import argparse
 
 
-async def fetch_url(url, session):
+async def fetch_url(session, url_gen, results):
     """
     Асинхронная функция, обкачивающая url, и возвращающая кортеж
     состоящий из URL'а и самого часто встречающегося слова. В случае
     если при запросе произошла ошибка, возвращается только URL
     """
-    try:
-        async with session.get(url) as response:
-            text = await response.read()
-            most_common_word = Counter(text.split()).most_common(1)
-            # в случае успешного запроса, вернём url и
-            # самое часто встречающиеся слово
-            return url, most_common_word
-    except aiohttp.ClientError as e:
-        # в случае ошибки при запросе к URL, вернём только url
-        return url
+    while True:
+        try:
+            url = next(url_gen)
+        except StopIteration:
+            break
+
+        try:
+            async with session.get(url) as response:
+                text = await response.read()
+                most_common_word = Counter(text.split()).most_common(1)
+                # в случае успешного запроса, вернём url и
+                # самое часто встречающиеся слово
+                results.append((url, most_common_word))
+        except aiohttp.ClientError:
+            # в случае ошибки при запросе к URL, вернём только url
+            results.append((url,))
 
 
-async def fetch_all(urls, concurrent_requests):
+async def fetch_all(urls_file, workers_number):
+    urls = url_gen(urls_file)
+
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        sem = asyncio.Semaphore(concurrent_requests)
+        fetch_results = []
 
-        async with sem:
-            for url in urls:
-                task = asyncio.create_task(fetch_url(url, session))
-                tasks.append(task)
+        workers = [
+            asyncio.create_task(fetch_url(session, urls, fetch_results))
+            for _ in range(workers_number)
+        ]
 
-        return await asyncio.gather(*tasks)
+        await asyncio.gather(*workers)
+
+    return fetch_results
 
 
 def url_gen(url_file):
@@ -65,7 +75,11 @@ if __name__ == "__main__":
     # с ней почему-то event loop закрывается раньше, чем надо
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    results = asyncio.run(fetch_all(url_gen(args.url_file), args.concurrent_requests))
+    start_time = time.time()
+    results = asyncio.run(fetch_all(args.url_file, args.concurrent_requests))
+    end_time = time.time() - start_time
 
-    for result in results:
-        print(result)
+    for res in results:
+        print(res)
+
+    print(f"Время выполнения: {end_time} секунд")
